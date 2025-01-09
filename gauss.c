1cfbfb6a3c21d80807d9fc012259d8bf4de35198
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-09 12:43:58 +0100

    Implemented swapRows

--- gauss.c
+++ gauss.c
@@ -1,2 +1,25 @@
 #include "gauss.h"
 
+void swapRows(Matrix* mat, size_t rowAId, size_t rowBId) {
+	double* rowA = (double*)calloc(mat->cols, sizeof(double));
+	double* rowB = (double*)calloc(mat->cols, sizeof(double));
+
+	getMatrixRow(mat, rowAId, rowA);
+	getMatrixRow(mat, rowBId, rowB);
+
+	setMatrixRow(mat, rowAId, rowB);
+	setMatrixRow(mat, rowBId, rowA);
+
+	free(rowA);
+	free(rowB);
+
+	return;
+}
+
+void subtractRows(Matrix* mat, size_t rowAId, size_t rowBId, double coeffRowB) {
+	return;
+}
+
+void multiplyRow(Matrix* mat, size_t rowId, double coeffRow) {
+	return;
+}
