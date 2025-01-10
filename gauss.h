8e91eca35e50edd7f04ad788719cb384f9b50963
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-10 14:41:30 +0100

    defined Swap struct to follow swaps made onto a matrix

--- gauss.h
+++ gauss.h
@@ -3,6 +3,10 @@
 #include "matrix.h"
 #include "common.h"
 
+typedef struct Swap {
+	size_t A, B;
+} Swap;
+
 void swapRows(Matrix* mat, size_t rowAId, size_t rowBId);
 void subtractRows(Matrix* mat, size_t rowAId, size_t rowBId, double coeffRowB);
 void multiplyRow(Matrix* mat, size_t rowId, double coeffRow);
