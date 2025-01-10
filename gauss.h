e9d598e581655f86b89ea32768e16e51653519de
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-10 14:42:00 +0100

    Declared function needed for Gauss pivot's algorithm

--- gauss.h
+++ gauss.h
@@ -10,3 +10,8 @@ typedef struct Swap {
 void swapRows(Matrix* mat, size_t rowAId, size_t rowBId);
 void subtractRows(Matrix* mat, size_t rowAId, size_t rowBId, double coeffRowB);
 void multiplyRow(Matrix* mat, size_t rowId, double coeffRow);
+
+size_t identifyGaussPivot(Matrix* mat, size_t rowId);
+
+size_t Partitioning(Matrix* mat, size_t firstId, size_t lastId, size_t pivotRowId, Swap* swaps, size_t* swapsMade);
+void QuickSort(Matrix* mat, size_t firstId, size_t lastId, Swap* swaps, size_t* swapsMade);
