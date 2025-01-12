a1f6709fe93e169dc58fbdd10d9c30a1a0b1ebca
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-13 00:37:33 +0100

    Mise à jour du script compile_test.sh pour ajouter la prise en charge de la compilation 64 bits et 32 bits avec les options de compilation C23.

--- compile_test.sh
+++ compile_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-gcc -Wall -std=c17 -Oz -s -masm=intel gauss.c matrix.c test.c -o test.elf
-gcc -std=c17 -S -Oz -s -masm=intel gauss.c matrix.c test.c
+gcc -Wall -m64 -march=x86-64 -std=c23 -Oz -s -masm=intel gauss.c matrix.c test.c -o test_x86-64.elf
+gcc -Wall -m32 -march=i386 -std=c23 -Oz -s -masm=intel gauss.c matrix.c test.c -o test_i386.elf
+
+gcc -std=c23 -S -Oz -s -masm=intel gauss.c matrix.c test.c
