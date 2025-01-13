0141f60578d54a14dceb365fac7730f730a50ffd
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-13 01:07:35 +0100

    Amélioration du script compile_test.sh pour générer des fichiers d'assemblage pour les architectures x86-64 et i386, avec des répertoires dédiés pour chaque architecture.

--- compile_test.sh
+++ compile_test.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-gcc -Wall -m64 -march=x86-64 -std=c23 -Oz -s -masm=intel gauss.c matrix.c test.c -o test_x86-64.elf
-gcc -Wall -m32 -march=i386 -std=c23 -Oz -s -masm=intel gauss.c matrix.c test.c -o test_i386.elf
+clear
 
-gcc -std=c23 -S -Oz -s -masm=intel gauss.c matrix.c test.c
+echo "Compiling for x86-64"
+gcc -w -m64 -march=x86-64 -std=c23 -O0 -masm=intel gauss.c matrix.c test.c -o test_x86-64.elf
+
+echo "Compiling for i386"
+gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel gauss.c matrix.c test.c -o test_i386.elf
+
+echo ""
+
+mkdir x86-64
+mkdir i386
+
+echo ""
+echo "Generating assembly files for x86-64"
+
+gcc -w -m64 -march=x86-64 -std=c23 -O0 -masm=intel -S gauss.c matrix.c test.c
+mv gauss.s matrix.s test.s test_x86-64.elf x86-64
+
+echo "Generating assembly files for i386"
+
+gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel -S gauss.c matrix.c test.c
+mv gauss.s matrix.s test.s test_i386.elf i386
+
+echo "ASM Files generated"
+echo ""
+
+file x86-64/* i386/*
