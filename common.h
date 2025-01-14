4b4fef74b50080ce22caea0c1127fcf1ca8fe5ef
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-14 16:09:38 +0100

    Remaking the transformation tracking system

--- common.h
+++ common.h
@@ -7,6 +7,12 @@
 #include <stdbool.h>
 #include <time.h>
 
+typedef enum TransformationType {
+	SWAP = 0,
+	SUB = 1,
+	MUL = 2
+} TransformationType;
+
 typedef enum SwapType {
 	Row = 0,
 	Column = 1
@@ -16,6 +22,15 @@ typedef struct Swap {
 	size_t A, B;
 	SwapType type;
 } Swap;
+
+typedef struct MatrixTransformation {
+	TransformationType transType;
+
+	size_t A, B;
+	value_t coeff;
+
+	SwapType swapType;
+} MatrixTransformation;
 
 #if defined(__x86_64__) || defined(_M_X64)
 #define VALUE_TYPE double
