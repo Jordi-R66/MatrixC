08a5e4b7b6c0b5e9f5a2d54abf9251198b1665be
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-17 23:50:36 +0100

    Mise à jour du script de compilation pour inclure les fichiers advMatrix.c et tracking.c

--- compile_test.sh
+++ compile_test.sh
@@ -3,10 +3,10 @@
 clear
 
 echo "Compiling for x86-64"
-gcc -w -D __64_BITS__ -m64 -march=x86-64 -std=c23 -O0 -masm=intel gauss.c matrix.c test.c -o test_x86-64.elf
+gcc -w -D __64_BITS__ -m64 -march=x86-64 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c -o test_x86-64.elf
 
 #echo "Compiling for i386"
-#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel gauss.c matrix.c test.c -o test_i386.elf
+#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c -o test_i386.elf
 #
 #echo ""
 #
@@ -16,12 +16,12 @@ gcc -w -D __64_BITS__ -m64 -march=x86-64 -std=c23 -O0 -masm=intel gauss.c matrix
 #echo ""
 #echo "Generating assembly files for x86-64"
 #
-#gcc -w -m64 -march=x86-64 -std=c23 -O0 -masm=intel -S gauss.c matrix.c test.c
+#gcc -w -m64 -march=x86-64 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c
 #mv gauss.s matrix.s test.s test_x86-64.elf x86-64
 #
 #echo "Generating assembly files for i386"
 #
-#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel -S gauss.c matrix.c test.c
+#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c
 #mv gauss.s matrix.s test.s test_i386.elf i386
 #
 #echo "ASM Files generated"
