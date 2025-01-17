f6866fcbef7cf98dcff466fabc0897c20b7d08c9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-17 22:24:59 +0100

    Changed the way to detect compilation target

--- common.h
+++ common.h
@@ -7,7 +7,20 @@
 #include <stdbool.h>
 #include <time.h>
 
-typedef enum TransformationType {
+#define VALUE_TYPE
+#define __64_BITS__
+
+#if defined(__64_BITS__)
+#define VALUE_TYPE double
+#elif defined(__32_BITS__)
+#define VALUE_TYPE float
+#else
+#error Unsupported target
+#endif
+
+typedef VALUE_TYPE value_t;
+
+enum TransformationType {
 	SWAP = 0,
 	SUB = 1,
 	MUL = 2
