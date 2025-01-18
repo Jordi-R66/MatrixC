102276ee9299770616d4564e1998e79fad8b73e4
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-18 01:58:17 +0100

    Ajout de la compilation pour les architectures x86-64 et i386 dans compile_test.sh, avec génération des fichiers d'assemblage correspondants.

--- compile_test.sh
+++ compile_test.sh
@@ -2,29 +2,39 @@
 
 clear
 
-echo "Compiling for x86-64"
-gcc -w -D __64_BITS__ -m64 -march=x86-64 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c -o test_x86-64.elf
+SRC_FILES="advMatrix.c tracking.c gauss.c matrix.c test.c"
+ASM_FILES="advMatrix.s tracking.s gauss.s matrix.s test.s"
 
-#echo "Compiling for i386"
-#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c -o test_i386.elf
-#
-#echo ""
-#
-#mkdir x86-64
-#mkdir i386
-#
-#echo ""
-#echo "Generating assembly files for x86-64"
-#
-#gcc -w -m64 -march=x86-64 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c
-#mv gauss.s matrix.s test.s test_x86-64.elf x86-64
-#
-#echo "Generating assembly files for i386"
-#
-#gcc -w -m32 -march=i386 -std=c23 -O0 -masm=intel advMatrix.c tracking.c gauss.c matrix.c test.c
-#mv gauss.s matrix.s test.s test_i386.elf i386
-#
-#echo "ASM Files generated"
-#echo ""
-#
-#file x86-64/* i386/*
+ASM_SYNTAX="-masm=intel"
+OPTIMIZATION="-O0"
+
+C_DIALECT="-std=c23"
+
+echo "Compiling for x86-64"
+gcc -g -w -D __64_BITS__ -m64 -march=x86-64 $C_DIALECT $OPTIMIZATION $ASM_SYNTAX $SRC_FILES -o test_x86-64.elf
+
+echo "Compiling for i386"
+gcc -g -w -D __32_BITS__ -m32 -march=i386 $C_DIALECT $OPTIMIZATION $ASM_SYNTAX $SRC_FILES -o test_i386.elf
+
+echo ""
+
+mkdir x86-64
+mkdir i386
+
+echo ""
+echo "Generating assembly files for x86-64"
+
+gcc -g -w -D __64_BITS__ -m64 -march=x86-64 $C_DIALECT $OPTIMIZATION $ASM_SYNTAX $SRC_FILES -S
+mv $ASM_FILES x86-64/
+mv test_x86-64.elf x86-64/
+
+echo "Generating assembly files for i386"
+
+gcc -g -w -D __32_BITS__ -m32 -march=i386 $C_DIALECT $OPTIMIZATION $ASM_SYNTAX $SRC_FILES -S
+mv $ASM_FILES i386/
+mv test_i386.elf i386/
+
+echo "ASM Files generated"
+echo ""
+
+file x86-64/* i386/*
